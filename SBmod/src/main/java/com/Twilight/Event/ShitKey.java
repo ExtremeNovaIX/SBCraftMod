099754733dc4dda4b02690f9f3d36ef4febe211b
MagicGoldenApple <Kevin25362@163.com>
2024-12-03 14:26:36 +0800

    实现喷屎功能

--- SBmod/src/main/java/com/Twilight/Event/ShitKey.java
+++ SBmod/src/main/java/com/Twilight/Event/ShitKey.java
@@ -1,23 +1,20 @@
 package com.Twilight.Event;
 
 import com.Twilight.Client.KeyBindings;
-import com.Twilight.ModBlock.HeadK2536Block;
-import com.Twilight.ModBlock.ModBLock;
 import com.Twilight.ModItems.ModItems;
 import net.minecraft.client.Minecraft;
-import net.minecraft.server.MinecraftServer;
-import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.entity.item.ItemEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.Level;
 import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.event.TickEvent;
+import net.minecraftforge.event.level.LevelEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.logging.Level;
+import java.util.Random;
 
 @Mod.EventBusSubscriber(modid = "sbmod", bus = Mod.EventBusSubscriber.Bus.FORGE, value = Dist.CLIENT)
 public class ShitKey {
@@ -25,27 +22,37 @@ public class ShitKey {
     private static final Logger log = LoggerFactory.getLogger(ShitKey.class);
 
     @SubscribeEvent
-    public static void onKeyInput(TickEvent.ServerTickEvent event) {
+    public static void onKeyInput(LevelEvent event) {
 
 
         Minecraft mc = Minecraft.getInstance();
         Player player = mc.player;
 
         if (player == null || !mc.isWindowActive()) return;
+        if (KeyBindings.INSTANCE.SHIT_KEY.isDown()) {
 
-        if (KeyBindings.INSTANCE.SHIT_KEY.consumeClick()) {
             double x = player.getX();
-            double y = player.getY();
+            double y = player.getY() + 2;
             double z = player.getZ();
-            if (mc.level.isClientSide()) {
-                player.displayClientMessage(player.getDisplayName().copy().append(" dropped some shit"), false);
+            if (event.getLevel().isClientSide()) {
 
-            }else{
+            } else {
                 ItemStack shitStack = new ItemStack(ModItems.SHIT.get(), 1);
-                for (int i = 0; i < 10; i++) {
-                    ItemEntity shitEntity = new ItemEntity(mc.level, x, y, z, shitStack);
-                    mc.level.addFreshEntity(shitEntity);
+
+                for (int i = 0; i < 1; i++) {
+                    ItemEntity itemEntity;
+                    itemEntity = new ItemEntity((Level) event.getLevel(), x, y, z, shitStack);
+                    Random random = new Random();
+                    itemEntity.setDeltaMovement(
+                            (random.nextDouble() - 0.5) * 0.5,
+                            random.nextDouble() * 0.8 + 0.2,
+                            (random.nextDouble() - 0.5) * 0.5
+                    );
+                    event.getLevel().addFreshEntity(itemEntity);
+
                 }
+
+
             }
         }
     }
